b63b3d7d1861c35850dc0d7851b341edc3b169e4
yusrilwahyuda <43510262+yusrilwahyuda@users.noreply.github.com>
2018-09-25 16:54:53 +0700

    Update formawal.php

--- formawal.php
+++ formawal.php
@@ -1,5 +1,15 @@
-<?php
 
-
-
-?>
+<h1>Hello</h1>
+<form action="proses.php" method="POST">
+	<table>
+	<tr>
+		<td>Nama</td><td><input type="text" name="nama[]"></td>
+	</tr>
+	<tr>
+		<td>Password</td><td><input type="password" name="password[]"></td>
+	</tr>
+	<tr>
+		<td><input type="submit" name="submit" value="submit"></td>
+	</tr>
+</table>
+</form>
